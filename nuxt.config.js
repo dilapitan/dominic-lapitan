e914aa8495476a44ed65f0fcda885b66aeafd779
dilapitan <domlapitan@gmail.com>
2022-04-22 16:29:21 +0800

    Update favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,6 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: "%s",
     title: "Dominic Lapitan",
     htmlAttrs: {
       lang: "en",
@@ -20,7 +19,7 @@ export default {
       { hid: "description", name: "description", content: "" },
       { name: "format-detection", content: "telephone=no" },
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/dilapitan.png" }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
