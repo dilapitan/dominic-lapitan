3883c39bd8f6b197930f36ad2ec8e7769f618bd2
dilapitan <domlapitan@gmail.com>
2022-04-22 15:53:45 +0800

    Initial commit

---
+++ layouts/error.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-app dark>
+    <template v-if="error.statusCode === 404">
+      <br />
+      <h2 class="red--text">
+        {{ pageNotFound }}.
+        <span class="black--text">Why you here, bro?</span>
+      </h2>
+    </template>
+
+    <h1 v-else>
+      {{ otherError }}
+    </h1>
+
+    <br />
+
+    <NuxtLink to="/">
+      <v-btn color="primary">Go Home</v-btn>
+    </NuxtLink>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'EmptyLayout',
+  layout: 'empty',
+  props: {
+    error: {
+      type: Object,
+      default: null,
+    },
+  },
+  data() {
+    return {
+      pageNotFound: 'Error 404. Not Found',
+      otherError: 'An error occurred',
+    }
+  },
+  head() {
+    const title =
+      this.error.statusCode === 404 ? this.pageNotFound : this.otherError
+    return {
+      title,
+    }
+  },
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,81 @@
+<template>
+  <v-container>
+    <br /><br />
+    <v-row justify="center" align="center">
+      <v-col cols="10" sm="8" md="6">
+        <div class="text-h4">DOMINIC LAPITAN</div>
+        <div class="text-h6 grey--text">Remove Software Developer</div>
+
+        <br />
+
+        <p>
+          Hello! My name is Dominic. Currently, I'm a remote software developer
+          working for
+          <span>
+            <a href="https://excellenceinbreeding.org/" target="_blank"
+              >Enterprise Breeding System</a
+            > </span
+          >. Aside from that, I love working on side projects and studying
+          topics related to modern Web development. On the side, I enjoy reading
+          <span>
+            <a
+              href="https://www.goodreads.com/user/show/121647616-dominic"
+              target="_blank"
+              >books</a
+            > </span
+          >,
+          <span>
+            <a href="https://en.wikipedia.org/wiki/Speedcubing" target="_blank"
+              >speedcubing</a
+            > </span
+          >, and playing classical pieces on the piano.
+        </p>
+      </v-col>
+    </v-row>
+
+    <v-row justify="center" align="center">
+      <v-col cols="10" sm="8" md="6">
+        <div class="d-flex justify-space-between flex-wrap">
+          <v-btn
+            v-for="(link, i) in links"
+            :key="i"
+            class="overline primary--text font-weight-bold"
+            text
+          >
+            {{ link.label }}
+          </v-btn>
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  data: () => ({
+    links: [
+      {
+        label: 'Portfolio',
+        link: '/',
+      },
+      {
+        label: 'Github',
+        link: 'https://github.com/dilapitan',
+      },
+      {
+        label: 'Codepen',
+        link: 'https://codepen.io/dilapitan',
+      },
+      {
+        label: 'LinkedIn',
+        link: 'https://www.linkedin.com/in/dilapitan/',
+      },
+      {
+        label: 'Buy Me Coffee',
+        link: 'https://ko-fi.com/dilapitan',
+      },
+    ],
+  }),
+}
+</script>
